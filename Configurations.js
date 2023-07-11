562ce3f9555d06f526da7e2af0a02e7e178832c5
Omiya <90664344+OmiduAnjane@users.noreply.github.com>
2023-07-11 16:03:33 +0530

    Update Configurations.js

--- Configurations.js
+++ Configurations.js
@@ -16,7 +16,7 @@ if (!gg) {
 
 global.owner = gg.split(",");
 global.mongodb = process.env.MONGODB || "mongodb+srv://queenlara:Dmoa0817.@queenlara.jz3uzcg.mongodb.net/?retryWrites=true&w=majority"; 
-global.sessionId = process.env.SESSION_ID || "pendragon";
+global.sessionId = process.env.SESSION_ID || "pendragonmds";
 global.prefa = process.env.PREFIX || ".";
 global.tenorApiKey = process.env.TENOR_API_KEY || "AIzaSyCyouca1_KKy4W_MG1xsPzuku5oa8W358c";
 global.packname = process.env.PACKNAME || `Pendragon Bot`;

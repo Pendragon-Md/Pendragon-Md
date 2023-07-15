0143b23b6fb1f1395ed393efcfea07ad0bfe3e0c
Omiya <90664344+OmiduAnjane@users.noreply.github.com>
2023-07-15 12:37:21 +0530

    Queen Amdi Asset

--- System/BotCharacters.js
+++ System/BotCharacters.js
@@ -1,60 +1,10 @@
-/* ---------------------------------------------------------------------------------/
-/                                                                                   /
-/             d8888 888    888                        888b     d888 8888888b.       /
-/            d88888 888    888                        8888b   d8888 888  "Y88b      /
-/           d88P888 888    888                        88888b.d88888 888    888      /
-/          d88P 888 888888 888  8888b.  .d8888b       888Y88888P888 888    888      /
-/         d88P  888 888    888     "88b 88K           888 Y888P 888 888    888      /
-/        d88P   888 888    888 .d888888 "Y8888b.      888  Y8P  888 888    888      /
-/       d8888888888 Y88b.  888 888  888      X88      888   "   888 888  .d88P      /
-/      d88P     888  "Y888 888 "Y888888  88888P'      888       888 8888888P"       /
-/                                                                                   / 
-/-----------------------------------------------------------------------------------/
-/ Author and Main Developer: FantoX                                                 /
-/ Github: https://github.com/FantoX001/Atlas-MD                                     /
-/ Powered By: Team ATLAS                                                            /
-/-----------------------------------------------------------------------------------/
-/             Meet Team ATLAS who holds all rights to this repository:              /
-/                                                                                   /
-/ 1. Pratyush - https://github.com/pratyush4932                                     /
-/ 2. Ahmii - https://github.com/Ahmii-kun                                           /               
-/ 3. Kai - https://github.com/Kai0071                                               /                    
-/ 4. Devime - https://github.com/Devime69                                           /
-/ 5. Jay JayOps - https://github.com/jayjay-ops                                     /
-/                                                                                   /
-/ ----------------------------------------------------------------------------------/
-/                                                                                   /
-/      With all of our hard work and defication you can enjoy this awesome bot!     /  
-/                                                                                   / 
-/----------------------------------------------------------------------------------*/
 
 
 
 /* ---Added Anime Character list---  /
- 0 --- Atlas MD
- 1 --- Power
- 2 --- Makima
- 3 --- Denji
- 4 --- Zero Two
- 5 --- Chika
- 6 --- Miku
- 7 --- Marin Kitagawa
- 8 --- Ayanokoji
- 9 --- Ruka
-10 --- Mizuhara
-11 --- Rem
-12 --- Sumi
-13 --- Kaguya
-14 --- Yumeko
-15 --- Kurumi
-16 --- Mai
-17 --- Yor
-18 --- Shinbou
-19 --- Eiko
-
-Image Source: https://wall.alphacoders.com/
-Image Source: https://wallpapercave.com/
-Video Host: https://graph.org/
+ 0 --- Pendragon MD
+ 1 --- Queen Amdi
+ 2 --- Sew Queen
 
 ** Note: 
 Do not change or remove present charID. You can add more characters by adding more charID sequentially.
@@ -77,13 +27,13 @@ global.charID0 = {
 
 global.charID1 = {
   botName : "Queen Amdi",
-  botVideo : "https://graph.org/file/b402e2d4718992187787b.mp4",
-  botImage1 : "https://wallpapercave.com/wp/wp11253614.jpg",
-  botImage2 : "https://wallpapercave.com/wp/wp9413488.jpg",
-  botImage3 : "https://wallpapercave.com/wp/wp11628561.jpg",
-  botImage4 : "https://wallpapercave.com/wp/wp11733443.jpg",
-  botImage5 : "https://wallpapercave.com/wp/wp11733435.jpg",
-  botImage6 : "https://wallpapercave.com/wp/wp11733458.jpg",
+  botVideo : "https://github.com/Pendragon-Md/Pendragon-Md/blob/main/Assets/ezgif-4-ec7eae50ff.mp4",
+  botImage1 : "https://i.ibb.co/7ggnCyr/68747470733a2f2f692e6962622e636f2f7233776d7077722f4c4f474f2e6a7067.jpg",
+  botImage2 : "https://i.ibb.co/7ggnCyr/68747470733a2f2f692e6962622e636f2f7233776d7077722f4c4f474f2e6a7067.jpg",
+  botImage3 : "https://i.ibb.co/7ggnCyr/68747470733a2f2f692e6962622e636f2f7233776d7077722f4c4f474f2e6a7067.jpg",
+  botImage4 : "https://i.ibb.co/7ggnCyr/68747470733a2f2f692e6962622e636f2f7233776d7077722f4c4f474f2e6a7067.jpg",
+  botImage5 : "https://i.ibb.co/7ggnCyr/68747470733a2f2f692e6962622e636f2f7233776d7077722f4c4f474f2e6a7067.jpg",
+  botImage6 : "https://i.ibb.co/7ggnCyr/68747470733a2f2f692e6962622e636f2f7233776d7077722f4c4f474f2e6a7067.jpg",
 }
 
 global.charID2 = {
